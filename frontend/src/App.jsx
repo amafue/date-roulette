7b68282bd84b64b612f2966d51d7fbcd8176b664
amanda <amandafuentes831@gmail.com>
2026-04-26 22:58:10 +0200

    feat: session detail logic applied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import SessionDetail from './pages/SessionDetail'
 import HistorySession from './pages/HistorySession'
 import CreateSession from './pages/CreateSession'
 import CreateChallenge from './pages/CreateChallenge'
-import ChallengeDetail from './pages/ChallengeDetail'
 import SignIn from "./pages/SignIn"
 import UserProfile from './pages/UserProfile'
 
@@ -20,7 +19,6 @@ const App = () => {
         <Route path="/sessions" element={<HistorySession/>}/>
         <Route path="/sessions/create" element={<CreateSession/>}/>
         <Route path="/challenges/create" element={<CreateChallenge/>}/>
-        <Route path="/challenges/:id" element={<ChallengeDetail/>}/>
       </Routes>
     </div>
   )
--- frontend/src/pages/SessionDetail.jsx
+++ frontend/src/pages/SessionDetail.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router'
+import { ArrowLeftIcon } from "lucide-react";
 
 const SessionDetail = () => {
+  const [session, setSession] = useState([])
+  const [loading, setLoading] = useState(false)
+  const {id} = useParams();
+  const getSessionDetail = async () => {
+    try {
+      setLoading(true);
+
+      const url = `http://localhost:5001/sessions/${id}`;
+      const res = await fetch(url)
+      const data = await res.json();
+
+      setSession(data);
+    } catch (error) {
+      console.error("Error fetching idea", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    getSessionDetail();
+  })
+
   return (
-    <div>SessionDetail</div>
+    <div>
+      <div className='container'>
+          <Link to={"/sessions"} className='link-btn'>
+            <ArrowLeftIcon  size={12}/>
+            Back to History
+          </Link>
+        </div>
+
+          <div className='card'>
+          {session && (
+            <div >
+                <h2>{session.challengeName}</h2>
+                <p>{session.notes}</p>
+                <small>{session.rating} • {session.date}</small>
+            </div>
+          )}
+          </div>
+          
+    </div>
   )
 }
 
